feda04f26bad1c6005899fa2350f84cd8c570cb2
Luis Enrique Rosas Espinoza <enriquerosas@lesscoded.dev>
2023-12-26 01:53:28 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Luis Enrique Rosas Espinoza on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
